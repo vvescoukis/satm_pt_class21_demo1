567413b6dddc512a7e6d58fa713621c871dd77f4
v.vescoukis@cs.ntua.gr <v.vescoukis@cs.ntua.gr>
2021-04-08 08:43:17 +0300

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
+/*
+ * -----------------------------------------------
+    clas21_demo1
+    Example with classes to represent 2 x 2 arrays
+   -----------------------------------------------
+*/
 
+#include <iostream>
 using namespace std;
 
-const int N = 2;
+const int N = 2;    // Do not change this!
 
-
+// --- class definition
 class myArray {
 private:
     float det;
@@ -16,11 +22,14 @@ public:
     void determinant();
 };
 
+// --- implementation of methods ---
+
+// --- Determinant
 void myArray::determinant() {
     det = data[0][0] * data[1][1] - data[0][1] * data[1][0];
 }
 
-
+// --- Constructor, reads from the console
 myArray::myArray() {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++) {
@@ -30,6 +39,7 @@ myArray::myArray() {
     determinant();
 }
 
+// --- Show data
 void myArray::myArrayShow() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -42,6 +52,7 @@ void myArray::myArrayShow() {
 }
 
 
+// === main ===
 int main() {
     myArray A1;
 
